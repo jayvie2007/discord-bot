1a1582f77fe54fbbd7f0794f7c0f008cd82f223d
Jayvie De Leon <jayvief.deleon@gmail.com>
2025-06-08 22:29:22 +0800

    fixed working bot

--- main.py
+++ main.py
@@ -93,4 +93,4 @@ async def remove_role(ctx, *, role_name: str):
         await ctx.send(f"no roles assigned {ctx.author.mention}")
 
 
-bot.run(local.DISCORD_TOKEN, log_handler=config.handler, log_level=logging.DEBUG)
+bot.run(local.DISCORD_TOKEN, log_handler=config.handler(), log_level=logging.DEBUG)
